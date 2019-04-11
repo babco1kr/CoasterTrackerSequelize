d7537bc8864e3cefd2b29ae969e66a0cfc3d8349
Kyle Babcock <kylerbabcock@gmail.com>
2019-04-10 20:25:37 -0400

    working with all sequelize functions

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,5 +11,28 @@ module.exports = function(app) {
         })
     })
 
-    app.put("api/coasters/:id", function (req, res))
+    app.put("/api/coasters/:id", function (req, res){
+        console.log(req.body.rode);
+        db.Coaster.update({
+            rode: req.body.rode
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(function(data){
+            if (data.changedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        })
+    })
+
+    app.post("/api/coasters", function(req, res) {
+        db.Coaster.create({
+            coaster_name: req.body.name
+        }).then(function(data) {
+            res.json({id: data.insertId});
+        })
+    })
 }
